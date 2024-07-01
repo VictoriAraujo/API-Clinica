2a69615abd1b335c0fcdcf0ff207e8e2f2fa8fa5
Maria Victória <126691192+VictoriAraujo@users.noreply.github.com>
2024-07-01 18:31:10 -0300

    up

---
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,26 @@
+package med.voll.api.controller;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.voll.api.medico.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("medicos")
+public class MedicoController {
+    @Autowired
+    private MedicoRepository repository;
+
+    @PostMapping
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
+        repository.save(new Medico(dados));
+    }
+    @GetMapping
+    public List<ListagemMedicos> listar(){
+        return repository.findAll().stream().map(ListagemMedicos::new).toList() ;
+    };
+}
---
+++ src/main/java/med/voll/api/medico/ListagemMedicos.java
@@ -0,0 +1,9 @@
+package med.voll.api.medico;
+
+public record ListagemMedicos(String nome, String email, String crm, Especialidade especialidade) {
+    public ListagemMedicos(Medico medico){
+        this(medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+
+    }
+
+}
